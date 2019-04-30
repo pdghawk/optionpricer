ec2884170a87500c0fe689458774fc612ac67b62
Peter Hawkins <27868344+pdghawk@users.noreply.github.com>
2019-04-30 14:29:44 -0400

    add some new payofff classes, and space to test them in test.py

--- optionpricer/payoff.py
+++ optionpricer/payoff.py
@@ -30,6 +30,93 @@ class CallPayOff:
 
     __repr__ = __str__
 
+
+class PutPayOff:
+    def __init__(self,strike):
+        if not isinstance(strike,(float,int)):
+            raise TypeError("PutPayOff object initialization: strike should be a float or an integer")
+        self._strike = strike
+        self._name   = "a put pay off with strike:"+str(self._strike)
+
+    def get_payoff(self,spot):
+        return np.maximum((self._strike-spot), 0.0)
+
+    def get_strike(self):
+        return self._strike
+
+    def __str__(self):
+        return self._name
+
+    __repr__ = __str__
+
+
+class DigitalPayOff:
+    def __init__(self,strike):
+        if not isinstance(strike,(float,int)):
+            raise TypeError("DisigitalPayOff object initialization: strike should be a float or an integer")
+        self._strike = strike
+        self._name   = "a digital pay off with strike:"+str(self._strike)
+
+    def get_payoff(self,spot):
+        if isinstance(spot, (float, int)):
+            if spot>self._strike:
+                return 1.0
+            else:
+                return 0.0
+        elif isinstance(spot, np.ndarray):
+            payoff = np.zeros_like(spot)
+            payoff[np.where(spot>self._strike)] = 1.0
+            return payoff
+        else:
+            raise TypeError("spot supplied to DigitalPayOff is of unsupported type")
+
+
+    def get_strike(self):
+        return self._strike
+
+    def __str__(self):
+        return self._name
+
+    __repr__ = __str__
+
+
+class DoubleDigitalPayOff:
+    def __init__(self,strike_lo,strike_hi):
+        condition = isinstance(strike_lo,(float,int)) and \
+                    isinstance(strike_hi,(float,int))
+        if not condition:
+            raise TypeError("DoubleDigitalPayOff object initialization: strikes should be float or integer")
+        self._strike_lo = strike_lo
+        self._strike_hi = strike_hi
+        self._name   = "a double digital pay off with strikes:"+str(self._strike_lo)+", "+str(self._strike_hi)
+
+    def get_payoff(self,spot):
+        if isinstance(spot, (float, int)):
+            if spot>self._strike_lo and spot<self._strike_lo:
+                return 1.0
+            else:
+                return 0.0
+        elif isinstance(spot, np.ndarray):
+            payoff = np.zeros_like(spot)
+            payoff[np.where(spot>self._strike_lo)] = 1.0
+            payoff[np.where(spot>self._strike_hi)] = 0.0
+            return payoff
+        else:
+            raise TypeError("spot supplied to DoubleDigitalPayOff is of unsupported type")
+
+
+    def get_strike(self,lo=True):
+        if lo:
+            return self._strike_lo
+        else:
+            return self._strike_hi
+
+    def __str__(self):
+        return self._name
+
+    __repr__ = __str__
+
+
 # class BadPayOff:
 #     def __init__(self,strike):
 #         if not isinstance(strike,(float,int)):
--- test.py
+++ test.py
@@ -61,33 +61,50 @@ print(str(call_option))
 # print(bspde.tridiag_constant(-2006.0, 1994012.0, -2006.0,initial_cond))
 
 ################################################################################
+
+# strike = 50.0
+# expiry = 1.0 #(18.0/12.0)
+#
+# call_payoff = payoff.CallPayOff(strike)
+#
+# call_option = option.VanillaOption(call_payoff,expiry)
+#
+# spotgrid = bspde.FinDiffGrid1D(1.0e-15,120.0,5000)
+# spotgrid.make_linear_grid()
+#
+# bcs = bspde.BoundaryConditions1D()
+# #bcs.set_lo_dirichlet(call_option.get_option_payoff(spotgrid.grid[0]))
+# #bcs.set_lo_dirichlet(0.0)
+# #bcs.set_hi_dirichlet(call_option.get_option_payoff(spotgrid.grid[-1]))
+# r = 0.024
+# sig = 0.12
+# bspricer = bspde.BlackScholesSingleAssetPricer(call_option,r,sig,bcs)
+#
+# spots=np.linspace(20.0,70.0,30)
+# # prices=[]
+# # bs_prices=[]
+# # for s in spots:
+# #     prices.append( bspricer.solve(s,spotgrid) )
+# #     bs_prices.append( analytics.black_scholes_call_price(s,strike,expiry,r,sig) )
+#
+# prices    = bspricer.solve(spots)
+# bs_prices = analytics.black_scholes_call_price(spots,strike,expiry,r,sig)
+# plt.plot(spots, prices, 'k')
+# plt.plot(spots, bs_prices, 'r')
+# plt.show()
+
+################################################################################
+
 strike = 50.0
-expiry = 1.0 #(18.0/12.0)
-
-call_payoff = payoff.CallPayOff(strike)
-
-call_option = option.VanillaOption(call_payoff,expiry)
-
-spotgrid = bspde.FinDiffGrid1D(1.0e-15,120.0,5000)
-spotgrid.make_linear_grid()
-
-bcs = bspde.BoundaryConditions1D()
-#bcs.set_lo_dirichlet(call_option.get_option_payoff(spotgrid.grid[0]))
-#bcs.set_lo_dirichlet(0.0)
-#bcs.set_hi_dirichlet(call_option.get_option_payoff(spotgrid.grid[-1]))
+expiry = 1.0
 r = 0.024
 sig = 0.12
-bspricer = bspde.BlackScholesSingleAssetPricer(call_option,r,sig,bcs)
 
+payoff = payoff.PutPayOff(strike)
+option = option.VanillaOption(payoff,expiry)
+bcs = bspde.BoundaryConditions1D()
+bspricer = bspde.BlackScholesSingleAssetPricer(option,r,sig,bcs)
 spots=np.linspace(20.0,70.0,30)
-# prices=[]
-# bs_prices=[]
-# for s in spots:
-#     prices.append( bspricer.solve(s,spotgrid) )
-#     bs_prices.append( analytics.black_scholes_call_price(s,strike,expiry,r,sig) )
-
 prices    = bspricer.solve(spots)
-bs_prices = analytics.black_scholes_call_price(spots,strike,expiry,r,sig)
 plt.plot(spots, prices, 'k')
-plt.plot(spots, bs_prices, 'r')
 plt.show()
